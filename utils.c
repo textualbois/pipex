6c7e7e36dca5391b11f7eecb08a2f8418b587f0d
textualbois <wivna360@hotmail.com>
2024-04-26 20:20:13 +0200

    utils.c

--- utils.c
+++ utils.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 20:11:50 by isemin            #+#    #+#             */
+/*   Updated: 2024/04/26 20:19:53 by isemin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 
 size_t	ft_arrlen(char **arr)
 {
-	size_t res;
+	size_t	res;
 
 	res = 0;
 	while (arr[res] != NULL)
@@ -30,8 +42,8 @@ void	*clear_arr(char **res_arr)
 
 char	**ft_map_s2a(char **arr, char *str, char *(*f)(const char*, const char*))
 {
-	char **res;
-	int	arr_i;
+	char	**res;
+	int		arr_i;
 
 	res = malloc((ft_arrlen(arr) + 1) * sizeof(char**));
 	arr_i = 0;
@@ -49,23 +61,23 @@ char	**ft_map_s2a(char **arr, char *str, char *(*f)(const char*, const char*))
 	return (res);
 }
 
-void	remove_chars(char  *str, char *c)
+void	remove_chars(char *str, char *c)
 {
-	char *char_pos;
-	int i;
+	char	*char_pos;
+	int		i;
 
 	i = 0;
 	if (str != NULL)
 	{
-		while(c[i] != '\0')
+		while (c[i] != '\0')
 		{
 			char_pos = ft_strchr(str, c[i]);
 			while (char_pos != NULL)
 			{
-				ft_strlcpy(char_pos, char_pos + 1,  ft_strlen(char_pos));
+				ft_strlcpy(char_pos, char_pos + 1, ft_strlen(char_pos));
 				char_pos = ft_strchr(str, c[i]);
 			}
-		i++;
+			i++;
 		}
 	}
 }
